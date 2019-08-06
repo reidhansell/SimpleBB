9f78f5d73f16c1c6617ad71dd8c577ca4db0d984
Reid Hansell <reidhansell@gmail.com>
2019-08-05 19:09:05 -0600

    Removed conflicting/unncessary files

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,7 +1,5 @@
 export const SET_ALERT = 'SET_ALERT';
 export const REMOVE_ALERT = 'REMOVE_ALERT';
-export const GET_ERRORS = 'GET_ERRORS';
-export const CLEAR_ERRORS = 'CLEAR_ERRORS';
 export const REGISTER_SUCCESS = 'REGISTER_SUCCESS';
 export const REGISTER_FAIL = 'REGISTER_FAIL';
 export const USER_LOADED = 'USER_LOADED';
