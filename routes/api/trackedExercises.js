dc34e4ac109d898ef0307dfe78f476c186b67fea
Reid Hansell <reidhansell@gmail.com>
2020-08-28 23:19:11 -0600

    Completed and tested tracked exercise routes with validation

--- routes/api/trackedExercises.js
+++ routes/api/trackedExercises.js
@@ -21,11 +21,10 @@ router.get(
     }
 
     const { date } = req.body;
-    console.log(date);
+
     try {
       const user = await User.findById(req.user.id);
-      console.log(user);
-      console.log("Attempting to find tracked exercises");
+
       const trackedExercises = await TrackedExercise.find({
         user: req.user.id,
         date: date,
@@ -35,7 +34,6 @@ router.get(
         trackedExercises = [];
       }
 
-      console.log(trackedExercises);
       res.json(trackedExercises);
     } catch (err) {
       res.status(500).send("Server Error");
@@ -51,29 +49,33 @@ router.post(
   [
     auth,
     [
-      //Holding off on validating ID inputs such as user and exercise because I am unsure how they're generated
+      //Holding off on validating ID inputs such as user and exercise because I am unsure how they're generated. Sets array is always initially empty
       check("exercises.*.date", "Date invalid").trim().isDate(),
     ],
   ],
   async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
     const { trackedExercises } = req.body;
-    console.log(trackedExercises);
-    console.log("begin adding exercises");
+
     try {
       const user = req.user.id;
       await trackedExercises.forEach((x) => {
         const { date, exercise } = x;
-        console.log(date);
+
         let trackedExercise = new TrackedExercise({
           user,
           date,
           exercise,
           sets: [],
         });
-        console.log("begin single save");
+
         trackedExercise.save();
       });
-      console.log("exercises saved");
+
       res.json({ success: true });
     } catch (err) {
       res.status(500).send("Server Error");
@@ -94,7 +96,7 @@ router.delete("/:id", auth, async (req, res) => {
       return res.status(404).json({ msg: "Tracked exercise not found" });
     }
 
-    await trackedExercise.remove(); //Confirm this doesn't need to be saved
+    await trackedExercise.remove();
 
     res.json({ success: true });
   } catch (err) {
@@ -105,28 +107,46 @@ router.delete("/:id", auth, async (req, res) => {
 // @route    POST api/trackedExercises/:id/sets
 // @desc     Add set to tracked exercise
 // @access   Private
-router.post("/:id/sets", [auth, []], async (req, res) => {
-  const { weightDistance, repsTime } = req.body;
+router.post(
+  "/:id/sets",
+  [
+    [
+      auth,
+      [
+        check("weightDistance", "Weight / Distance must be a valid integer")
+          .trim()
+          .isInt(),
+        check("repsTime", "Reps / Time must be a valid integer").trim().isInt(),
+      ],
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { weightDistance, repsTime } = req.body;
 
-  const newSet = {
-    weightDistance,
-    repsTime,
-  };
+    const newSet = {
+      weightDistance,
+      repsTime,
+    };
 
-  try {
-    const trackedExercise = await TrackedExercise.findOne({
-      _id: req.params.id,
-    });
+    try {
+      const trackedExercise = await TrackedExercise.findOne({
+        _id: req.params.id,
+      });
 
-    await trackedExercise.sets.unshift(newSet);
+      await trackedExercise.sets.unshift(newSet);
 
-    await trackedExercise.save();
+      await trackedExercise.save();
 
-    res.json({ success: true });
-  } catch (err) {
-    res.status(500).send("Server Error");
+      res.json({ success: true });
+    } catch (err) {
+      res.status(500).send("Server Error");
+    }
   }
-});
+);
 
 // @route    DELETE api/trackedExercises/:exerciseid/:setid
 // @desc     Delete a set
@@ -136,11 +156,11 @@ router.delete("/:id/:setid", auth, async (req, res) => {
     const trackedExercise = await TrackedExercise.findOne({
       _id: req.params.id,
     });
-    console.log(trackedExercise);
+
     const set = await trackedExercise.sets.find((x) => {
       return x.id === req.params.setid;
     });
-    console.log(set);
+
     if (!set) {
       return res.status(404).json({ msg: "Set not found" });
     }
